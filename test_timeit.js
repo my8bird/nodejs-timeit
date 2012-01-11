bc4fbc60dca32418792b342e3dfef6ec6e5ef97a
Nathan Landis <my8bird@gmail.com>
2012-01-10 20:23:17 -0600

    Use a different method for running the steps that allows more granular measurements.  Update to have various metrics returned.

--- test_timeit.js
+++ test_timeit.js
@@ -1,23 +1,51 @@
 var timeit = require('./timeit'),
     assert = require('assert');
 
-describe('Timing function iteration is fun', function(){
-   var iterations = 30;
+assert.howlongValuesSet = function(values) {
+   assert.ok(undefined !== values.total_runtime);
+   assert.ok(undefined !== values.total_step_runtime);
+   assert.ok(undefined !== values.average_step_runtime);
+   assert.ok(undefined !== values.total_off_baseline);
+   assert.ok(undefined !== values.total_step_off_baseline);
+   assert.ok(undefined !== values.average_step_off_baseline);
+};
+
+
+describe('Timing functions', function() {
+   before(function(done) {
+      timeit.setBaseline(done);
+   });
+
    it('should run the function the correct number of times.', function(done) {
-      timeit.howlong(iterations, function(done) {
-         setTimeout(function(){ done(); }, 10);
-      }, function(duration) {
-         if (duration <= 0) {throw 'duration should be positive';};
-         done();
-      });
+      timeit.howlong(30,
+                     function(done) {
+                        setTimeout(function(){ done(); }, 10);
+                     },
+                     function(err, values) {
+                        assert.ok(values.total_runtime > 0);
+                        assert.howlongValuesSet(values);
+                        done();
+                     }
+                    );
    });
 
    it('should not blow the stack .', function(done) {
-      var iterations = 30000;
-      timeit.howlong(iterations, function(done) {
-         done();
-      }, function(duration) {
-         if (duration <= 0) {throw 'duration should be positive';};
+      timeit.howlong(30000,
+                     function(done) {
+                        done();
+                     },
+                     function(err, values) {
+                        assert.ok(values.total_runtime > 0);
+                        assert.howlongValuesSet(values);
+                        done();
+                     }
+                    );
+   });
+
+   it('should produce a baseline when requested', function(done) {
+      timeit.setBaseline(function(err, values) {
+         assert.ok(values.total_runtime > 0);
+         assert.howlongValuesSet(values);
          done();
       });
    });
--- timeit.js
+++ timeit.js
@@ -1,15 +1,5 @@
-var //process = require('process'),
-
-    active        = false,
-    howlong_calls = [];
-
-function _startTimeit(howlong_args) {
-   active = true;
-};
-
-function _endTimeit() {
-   active = false;
-};
+var EventEmitter = require('events').EventEmitter,
+    _baseline = null;
 
 
 function _howlong(iterations, func, done) {
@@ -28,22 +18,68 @@ function _howlong(iterations, func, done) {
    func(when_iter_done);
 }
 
-exports.howlong = function(/* @see _howlong */) {
-   var whendone  = arguments[arguments.length-1],
-       args      = Array.prototype.slice.call(arguments, 0, arguments.length-1),
-       starttime;
+exports.setBaseline = function(whendone) {
+   exports.howlong(30000,
+                   // Use the fastest possible function
+                   function(done) {
+                      done();
+                   },
+                   // Store the values for later use and return to caller
+                   function(err, values) {
+                      _baseline = values;
+                      whendone(err, values);
+                   },
+                   true
+                  );
+};
 
-   if (active) {
-      howlong_calls.push(arguments);
-      return;
+exports.howlong = function(iterations, func, done, ignoreWarning) {
+   var starttime = new Date(),
+       emitter   = new EventEmitter(),
+       i = 0,
+       step_values = [];
+
+   if (! _baseline && ignoreWarning === undefined ) {
+      console.log('Run setBaseline before howlong to get more accurate results.');
    }
 
-   // Add the method to call when the loop is done.
-   args.push(function () {
-      var endtime = new Date();
-      whendone(endtime - starttime);
+   function _runStep() {
+      process.nextTick(function() {
+         var step_start = new Date();
+         func(function() {
+            step_values.push(new Date() - step_start);
+            emitter.emit('step_done');
+         });
+      });
+   }
+
+   function _calcAndReturn() {
+      var runtime = new Date() - starttime,
+          total = step_values.reduce(function(value, item) { return value + item; }),
+          average = total / iterations,
+          results = {
+             total_runtime:        runtime,
+             total_step_runtime:   total,
+             average_step_runtime: average
+          };
+
+      if (_baseline) {
+         results.total_off_baseline        = runtime - _baseline.total_runtime;
+         results.total_step_off_baseline   = total   - _baseline.total_step_runtime;
+         results.average_step_off_baseline = average - _baseline.average_step_runtime;
+      }
+
+      done(null, results);
+   }
+
+   emitter.on('step_done', function() {
+      if (++i > iterations) {
+         _calcAndReturn();
+      } else {
+         _runStep();
+      }
    });
 
-   starttime = new Date();
-   _howlong.apply(null, args);
+   // Kick it all off
+   _runStep();
 };
